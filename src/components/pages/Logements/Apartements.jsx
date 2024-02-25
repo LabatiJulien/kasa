26c4d034eee0f6e41f511e307125b3580d729fd1
LabatiJulien <julien.labati@hotmail.fr>
2024-02-25 13:12:49 +0100

    Récupération du title/id pour Apartements

--- src/components/pages/Logements/Apartements.jsx
+++ src/components/pages/Logements/Apartements.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Apartements() {
+  const { id } = useParams();
+  const [apartmentTitle, setApartmentTitle] = useState(null);
+
+  useEffect(() => {
+    const fetchApartmentTitle = async () => {
+      try {
+        const response = await fetch(`/logements.json`);
+        const data = await response.json();
+        const apartment = data.find(apartment => apartment.id === id);
+        if (apartment) {
+          setApartmentTitle(apartment.title);
+        } else {
+          console.error('L\'appartement avec l\'ID spécifié n\'a pas été trouvé.');
+        }
+      } catch (error) {
+        console.error('Erreur lors du chargement du titre de l\'appartement:', error);
+      }
+    };
   
-  const { title } = useParams();
+    fetchApartmentTitle();
+  }, [id]);
 
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>{apartmentTitle}</h2>
     </div>
   );
 }
